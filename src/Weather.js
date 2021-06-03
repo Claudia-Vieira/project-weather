c8915a0cc6c8c00b210619996001e8d83766e7b5
Claudia Vieira <clau_f.v@hotmail.com>
2021-06-03 16:02:22 +0100

    get the value from the form and insert in h2

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Weather (){
+
+    let [city, setCity] = useState(null);
+
+    function showCity(event){
+        setCity(event.target.value);
+    }
+
     return (
 <div>
     <h1> Weather App</h1>
      <form>
-         <input type="text" placeholder="Enter a city..."/>
+         <input type="text" placeholder="Enter a city..." onChange={showCity}/>
          <input type="submit" value="Search"/>
      </form>
+
+     <h2>{city}</h2>
 </div>
     )
 };
