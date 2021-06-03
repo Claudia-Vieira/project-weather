413539b1d722a79632bdf1668dd355404b5698d8
Claudia Vieira <clau_f.v@hotmail.com>
2021-06-03 16:16:43 +0100

    show information only after submiting the form

--- src/Weather.js
+++ src/Weather.js
@@ -4,19 +4,32 @@ export default function Weather (){
 
     let [city, setCity] = useState(null);
 
+    let [weatherInformation, setWeatherInformation] = useState(null);
+
     function showCity(event){
         setCity(event.target.value);
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+
+        setWeatherInformation(
+            <h2>
+                {city}
+            </h2>
+        )
+
     }
 
     return (
 <div>
     <h1> Weather App</h1>
-     <form>
+     <form onSubmit={handleSubmit}>
          <input type="text" placeholder="Enter a city..." onChange={showCity}/>
          <input type="submit" value="Search"/>
      </form>
 
-     <h2>{city}</h2>
+     <h2>{weatherInformation}</h2>
 </div>
     )
 };
