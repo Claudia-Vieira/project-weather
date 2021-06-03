65d4737f8fba14f78f1adf37b4899496c34d38b2
Claudia Vieira <clau_f.v@hotmail.com>
2021-06-03 17:30:54 +0100

    Set date and hours

--- src/Date.js
+++ src/Date.js
@@ -1,10 +1,39 @@
 import React from "react";
 
-export default function Date(){
+export default function setDate(){
+
+    let today=new Date();
+
+    let month = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ];
+      
+    let date = [
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat"
+      ];
+
+
     return(
         <div>
-        <p>Hours</p>
-        <p>Day</p>
+        <p>{date[today.getDate()]},{today.getDay()}th {month[today.getMonth()]}</p>
+        <p>{today.getHours()}:{today.getMinutes()}</p>
         </div>
     )
 }
