6e5aa3ee0b9b6ddd811dd3ef810bd6412c81d3c6
Claudia Vieira <clau_f.v@hotmail.com>
2021-06-03 15:49:08 +0100

    add form

---
+++ src/Weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Weather (){
+    return (
+<div>
+    <h1> Weather App</h1>
+     <form>
+         <input type="text" placeholder="Enter a city..."/>
+         <input type="submit" value="Search"/>
+     </form>
+</div>
+    )
+};
